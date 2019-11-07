54af634081922d7a8b192d41db779e0eb7a21249
lay-coder <nanmucoffin@163.com>
2019-11-07 23:22:25 +0800

    init

--- src/components/ChildrenTwo.jsx
+++ src/components/ChildrenTwo.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
+import { Button } from 'element-react'
 import GrandSonTwo from './GrandSonTwo'
 
 export default class ChildrenTwo extends React.Component {
     render() {
         return <div style={{ backgroundColor: "blue" }}>二儿子
+        <Link to="/two/grandson"><Button>跳转二孙子</Button></Link>
             <Route path='/two/grandson' component={GrandSonTwo}></Route>
         </div>
     }
