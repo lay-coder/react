4d2b58884a3ddfcd32c9947e8661cd4ce1a2604a
lay-coder <nanmucoffin@163.com>
2019-11-08 16:52:09 +0800

    update

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import 'element-theme-default'
 import './index.scss'
 // import APP from './router'
 import * as serviceWorker from './serviceWorker'
 import Parent from 'components/Parent'
-
-ReactDOM.render(<BrowserRouter ><Parent /></BrowserRouter >, document.getElementById('root'))
+const routes = [
+  {
+    path: '/',
+    component: Parent,
+  },
+]
+const APP = () => (
+  <BrowserRouter history={}>
+    {routes.map((route, i) => (
+      <Route
+        key={i}
+        path={route.path}
+        render={<route.component {...prop} routes={route.routes} />}
+      ></Route>
+    ))}
+  </BrowserRouter>
+)
+ReactDOM.render(<APP />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
