19c70458642c6389f6c1451816afc0ddaf5f6525
lay-coder <nanmucoffin@163.com>
2019-11-19 19:02:50 +0800

    update

--- src/views/show/components/map/index.jsx
+++ src/views/show/components/map/index.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 export default class Map extends React.Component {
+  constructor(prpos) {
+    super(prpos)
+    this.state = {
+      map: {}
+    }
+  }
   componentDidMount() {
-    new window.AMap.Map('container', {
+    var map = new window.AMap.Map('container', {
       mapStyle: 'amap://styles/a1d6d07ee1e837f7a6dec11debea7de3',
       zoom: 14,//级别
       zooms: [14, 20],
       center: [121.47495, 31.219963],
       viewMode: '3D'
-    });
+    })
+    this.setState({
+      map: map
+    })
+    console.log(this.state.map)
   }
-
   render() {
     return (
       <div id="container" style={{ width: '100%', height: '100%' }}></div>
